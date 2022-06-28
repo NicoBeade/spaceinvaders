76835fa1fd7af78f4c3916757acc71e70135cf46
eloprestoripa <108023877+eloprestoripa@users.noreply.github.com>
2022-06-28 18:45:26 -0300

    InputAllegro
    Primera version del archivo inputAllegro.c

--- inputAllego.c
+++ inputAllego.c
@@ -13,8 +13,94 @@
 *   Este archivo contiene las funciones y threads encargadas de todas las acciones relacionadas a recibir input del usuario cuando el juego se ejecuta a.
 *
  **********************************************************************************************************************************************************/
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <pthread.h>
 
+static enum MYKEYS { KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT };
 
- void * getInput(){
+void keybord(void * arg){
+    
+    ALLEGRO_EVENT_QUEUE * event_queue = * (ALLEGRO_EVENT_QUEUE *) arg;
+    ALLEGRO_EVENT ev;
 
- }
+    bool fail = false;
+    bool do_exit = false;
+    bool key_pressed[4] = {false, false, false, false};
+
+    /**********************************************************************************************************
+     *  
+     *                                          INICIALIZACION
+     * 
+     * *******************************************************************************************************/
+
+    if (!al_install_keyboard() && !fail) {
+        fprintf(stderr, "failed to initialize the keyboard!\n");
+        fail = true;
+    }
+
+    //Si falla la inicializacion
+
+    if (fail){
+        pthread_exit(NULL);
+    }
+
+    //Se registra el display en la cola de eventos
+
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    while (!do_exit) {
+
+        if (al_get_next_event(event_queue, &ev)) 
+        {
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+                do_exit = true;
+
+            else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+                switch (ev.keyboard.keycode) {
+                    case ALLEGRO_KEY_UP:
+                        key_pressed[KEY_UP] = true;
+                        break;
+
+                    case ALLEGRO_KEY_DOWN:
+                        key_pressed[KEY_DOWN] = true;
+                        break;
+
+                    case ALLEGRO_KEY_LEFT:
+                        key_pressed[KEY_LEFT] = true;
+                        break;
+
+                    case ALLEGRO_KEY_RIGHT:
+                        key_pressed[KEY_RIGHT] = true;
+                        break;
+                }
+            }
+
+            else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
+                switch (ev.keyboard.keycode) {
+                    case ALLEGRO_KEY_UP:
+                        key_pressed[KEY_UP] = false;
+                        break;
+
+                    case ALLEGRO_KEY_DOWN:
+                        key_pressed[KEY_DOWN] = false;
+                        break;
+
+                    case ALLEGRO_KEY_LEFT:
+                        key_pressed[KEY_LEFT] = false;
+                        break;
+
+                    case ALLEGRO_KEY_RIGHT:
+                        key_pressed[KEY_RIGHT] = false;
+                        break;
+
+                    case ALLEGRO_KEY_ESCAPE:
+                        do_exit = true;
+                        break;
+                }
+            }
+        }
+
+    }
+
+    pthread_exit(NULL);
+}
