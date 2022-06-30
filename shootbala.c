04104bcc760dc23b9d1959e49cca8baf83f70567
nbeade <nbeade@itba.edu.ar>
2022-06-30 14:17:58 -0300

    PROGRESO EN BALAS Y ALIENS

--- shootbala.c
+++ shootbala.c
@@ -91,4 +91,65 @@ unsigned int countList(object_t * lista){  //Cuenta la cantidad de nodos de una 
 //}object_t;
 
 
-object_t initBarreras(int cantBarreras)
+object_t initBarreras(level_setting_t * levelSetting, int cantBarreras, char * str, ...){
+    va_list typeMiniBarrera;        //Puntero a argumentos variables
+    va_start(typeMiniBarrera, str);    //Se inicializan los argumentos variables, tipo de minibarrera
+    object_t * barreras = NULL;               //Se crea variable que almacenara la lista de barreras
+    vector_t posicionBarrera = {levelSetting->barreraInicialX,levelSetting->barreraInicialY};         //Se crea variable que almacenara pos de minibarreras
+    int barrera;                                        //Contador de barreras
+    int miniBarrera;                                    //Contador de minibarreras
+    char * string = str;                                //String de entrada
+    types_t tipoMini = NONE;                            //Tipo de minibarrera
+    int columnas = 0;                                   //Cantidad de minibarreras en una fila
+    int filas = 1;                                      //Cantidad de filas de minibarreras
+    int err = 0;                                        //Variable de error (0 no hubo error)
+    miniBarrierType_t tipos[MAXCANTINPUT] = {};         //Array auxiliar de tipos y sus identificadores de letras relleno con ceros
+    for(letra = 0; string[letra] != '\0'; letra++){     //Se recorre el string para revisar que la entrada sea correcta
+        char caracter = string[letra];
+        err = !(CHECK_LETRA(caracter) || caracter == '\n' || caracter == ' ');               //Si la entrada no es valida hubo error
+        if(err == 0){                                   //Si no hay error
+            if(caracter == '\n'){                  //Si el caracter es un enter
+                filas++;                                //Se agrega una fila
++           }
+            else{                                       //Si no es un enter
+                int c;
+                for(c = 0; (caracter != tipos[c].id) && (tipos[c].id != 0); c++);        //Chequea si el tipo fue agregado al array auxiliar
+                if(caracter != tipos[c].id){                                    //Si el caracter no fue agregado lo agrega
+                    tipos[c].id = caracter;                                     //Agrega en orden las letras
+                    if(caracter == ' '){                                        //Si el caracter es un espacio
+                        tipos[c].tipo = NONE;
+                    }
+                    else{
+                        tipos[c].tipo = va_arg(typeMiniBarrera, int);
+                    }
+                }                                     
+            }
+        }
+    }
+
+    for(barrera=0; barrera < cantBarreras; barrera++){    //Por cada barrera
+        for(miniBarrera = 0; miniBarrera < cantMiniBarreras; miniBarrera++){    //Por cada minibarrera
+            tipoMini = va_arg(typeMiniBarrera);
+            switch(tipoMini){
+                case: BARRERA_ESQUINA_INF_IZQ
+                case: BARRERA_ESQUINA_INF_DER
+                case: BARRERA_ESQUINA_SUP_IZQ
+                case: BARRERA_ESQUINA_SUP_DER
+                    break;
+                default:
+                    return NULL;
+            }
+            barreras = addObj(barreras, posicionBarrera, tipoMini, levelSetting->miniBarreraLives);
+            posicionBarrera.x;
+            //FUNCION A MEDIO HACER
+            // AGREGAR STRING QUE DIGA ("ABBC BBBB DBBE") con A B C D E distintos tipos de barreras
+        }
+    }
+    return barreras;
+}
+
+typedef struct MINIBARRIERTYPE{
+    char id;
+    types_t tipo;
+}miniBarrierType_t
+
