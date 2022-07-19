b741c776ef4738f908f0eb867e9fbd9ecb058229
fditoro <fditoro@itba.edu.ar>
2022-07-18 23:57:06 -0300

    Prueba menues 22

--- inputRaspi.c
+++ inputRaspi.c
@@ -35,7 +35,7 @@ void* inputRPIThread(void* argInputRPI){
             joy_update();
             coordJoy = joy_get_coord();//Obtiene las coordenadas del joystick.
             switchJoy =  joy_get_switch();
-
+            /*
             if(coordJoy.x <= JOY_ACTIVE_NEG){
                 ((keys_t*)argInputRPI) -> x = -1;
             }
@@ -62,11 +62,10 @@ void* inputRPIThread(void* argInputRPI){
             else{
                 ((keys_t*)argInputRPI) -> press = 0;
             }
-
-            printf("Moove: %d\n", ((keys_t*)argInputRPI) -> x);
-            printf("Press: %d\n", ((keys_t*)argInputRPI) -> press);
+            */
             
-            /*
+            
+            
             //Si el joystick se mueve, indico ese valor en los campos de la variable keys.
             ((keys_t*)argInputRPI) -> x = (coordJoy.x <= JOY_ACTIVE_NEG) ? -1 : ( (coordJoy.x >= JOY_ACTIVE_POS) ? 1 : 0); 
 
@@ -74,7 +73,9 @@ void* inputRPIThread(void* argInputRPI){
 
             //Esta seccion detecta si se presiono el boton del joystick.
             ((keys_t*)argInputRPI) -> press = (switchJoy == J_PRESS) ? 1 : 0;
-            */
+            
+            printf("Moove: %d\n", ((keys_t*)argInputRPI) -> x);
+            printf("Press: %d\n", ((keys_t*)argInputRPI) -> press);
         }
     }
     pthread_exit(0);
