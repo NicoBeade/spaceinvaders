d29330cf8218cc1994591eb032fbc76665a1efa8
fditoro <fditoro@itba.edu.ar>
2022-07-20 23:21:56 -0300

    Prueba menu completo 27

--- displayRaspi.c
+++ displayRaspi.c
@@ -925,7 +925,7 @@ void* textAnimMenu(void* argTextAnimMenu){
 //Este thread es el que se encarga de realizar el barrido de texto durante la ejecucion de un menu.
 
     printf("Texto dentro del thread de barrido: %s\n", ((argTextAnimMenu_t*)argTextAnimMenu) -> msg);
-    printf("Puntero al display dentro del thread de barrido: %s\n", ((argTextAnimMenu_t*)argTextAnimMenu) -> lowerDispMenu );
+    printf("Puntero al display dentro del thread de barrido: %p\n", ((argTextAnimMenu_t*)argTextAnimMenu) -> lowerDispMenu );
     
     int i, j, offset;
     int firstBarr = 4;
