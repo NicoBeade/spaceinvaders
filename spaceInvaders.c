f761d20f9c068af05b80dbd9a0688eb3923fd42d
fditoro <fditoro@itba.edu.ar>
2022-10-02 19:52:43 -0300

    Prueba de integracion 19

--- spaceInvaders.c
+++ spaceInvaders.c
@@ -493,7 +493,7 @@ static void* levelHandlerThread(void * data){
 void * moveAlienThread(void* argMoveAlien){
     //Este thread se encarga de mover la posicion de los aliens teniendo en cuenta para ello la variable direccion.
     
-    int static direccion = DERECHA; //Determina la direccion en la que se tienen que mover los aliens en el proximo tick
+    static int direccion = DERECHA; //Determina la direccion en la que se tienen que mover los aliens en el proximo tick
     while(1){
         usleep(10 * U_SEC2M_SEC);//Espera 10mS para igualar el tiempo del timer.
         if( (timerTick % velAliens) == 0 ){
