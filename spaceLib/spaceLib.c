4e54ac35a29b6755334628084c9650024594ca61
Fditoro <fditoro@itba.edu.ar>
2022-10-23 17:32:31 -0300

    Prueba barreras 26

--- spaceLib/spaceLib.c
+++ spaceLib/spaceLib.c
@@ -463,7 +463,6 @@ void collider(level_setting_t * levelSettings, object_t ** alienList, object_t *
                     listBalasEnemigas = listBalasEnemigas->next;//Apunta a la siguiente bala
 
                     *balasEnemigas = destroyObj(*balasEnemigas, balaADestruir);
-
                     printLista(*balasEnemigas, "Balas Jeremy");
                 }
                 else{//Si la bala no debe morir
@@ -474,7 +473,7 @@ void collider(level_setting_t * levelSettings, object_t ** alienList, object_t *
                 listBarreras = listBarreras->next;
             }
         }
-        if(collision(listBalasEnemigas->pos, listBalasEnemigas->type, listUsr->pos, listUsr->type) && collition){
+        if(collision(listBalasEnemigas->pos, listBalasEnemigas->type, listUsr->pos, listUsr->type) && collition && listBalasEnemigas != NULL){
 
             collition = 0;
             listUsr->lives -= 1;//Si una bala golpeo al usuario se le quita una vida.
@@ -494,7 +493,7 @@ void collider(level_setting_t * levelSettings, object_t ** alienList, object_t *
                 listBalasEnemigas = listBalasEnemigas->next;//Apunta a la siguiente bala
             }
         }
-        else if(collition){//Si no hubo colision
+        else if(collition && listBalasEnemigas != NULL){//Si no hubo colision
             listBalasEnemigas = listBalasEnemigas->next;//Apunta a la siguiente bala
         }
         
