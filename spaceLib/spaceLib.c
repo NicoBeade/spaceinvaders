119fb5eeaa50674df9ca764e2a38bc7fd80b10fe
Fditoro <fditoro@itba.edu.ar>
2022-10-29 10:57:59 -0300

    Error collider

--- spaceLib/spaceLib.c
+++ spaceLib/spaceLib.c
@@ -396,15 +396,15 @@ object_t * shootBala(object_t * listaNaves, object_t * listaBalas, level_setting
  * 
  ******************************************************************************************************************************************/
 
-void moveNaveUsuario(object_t * naveUsuario, level_setting_t* levelSettings, int direccion){
+void moveNaveUsuario(object_t ** naveUsuario, level_setting_t* levelSettings, int direccion){
 /* Esta funcion se llama como callback por los threads que manejan el input tanto en allegro como en la raspberry. Se encarga de actualizar
     la posicion de la nave del usuario.
 */
-    if( direccion == DERECHA && (naveUsuario -> pos.x <= levelSettings->xMax - levelSettings->anchoUsr) ){//Si se tiene que mover para la derecha y no llego al limite
-        naveUsuario -> pos.x += levelSettings->desplazamientoUsr;//Desplaza la nave
+    if( direccion == DERECHA && ((*naveUsuario) -> pos.x <= levelSettings->xMax - levelSettings->anchoUsr) ){//Si se tiene que mover para la derecha y no llego al limite
+        (*naveUsuario) -> pos.x += levelSettings->desplazamientoUsr;//Desplaza la nave
     }
-    else if( direccion == IZQUIERDA && (naveUsuario -> pos.x != levelSettings->xMin) ){//Si se tiene que mover para la izquierda y no llego al limite
-        naveUsuario -> pos.x -= levelSettings->desplazamientoUsr;//Desplaza la nave
+    else if( direccion == IZQUIERDA && ((*naveUsuario) -> pos.x != levelSettings->xMin) ){//Si se tiene que mover para la izquierda y no llego al limite
+        (*naveUsuario) -> pos.x -= levelSettings->desplazamientoUsr;//Desplaza la nave
     }
 }
 
