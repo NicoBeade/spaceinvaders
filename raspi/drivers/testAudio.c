8ddd9a350c7465db6174634bfe8a44c3d972f94f
Fditoro <fditoro@itba.edu.ar>
2022-10-29 13:51:47 -0300

    Prueba audio 14

--- raspi/drivers/testAudio.c
+++ raspi/drivers/testAudio.c
@@ -36,13 +36,37 @@ void* soundThread(void* audio){
     while(1){};
 }
 
+void* soundThread1(void* audio){
+    sleep(1);
+
+    printf("Audio selected: %s\n", (char*)audio);
+
+    init_sound();
+
+    if(player_status()==READY){
+
+        printf(GREEN_TEXT "Playing: %s \n",(char*)audio); 	printf(WHITE_TEXT);
+	
+		set_file_to_play((char*)audio);      // Load file 			
+	
+		play_sound(); 
+    }
+    else{
+			
+		printf(RED_TEXT "System not ready did you run init_sound() ? \n" WHITE_TEXT );		
+        pthread_exit(0);		
+	}
+
+    while(1){};
+}
+
 int main(void){
 
     pthread_create(&soundT, NULL, soundThread, AudioFile);
 
     sleep(1);
 
-    pthread_create(&soundT1, NULL, soundThread, Booo);
+    pthread_create(&soundT1, NULL, soundThread1, Booo);
 
     pthread_join(soundT, NULL);
 
