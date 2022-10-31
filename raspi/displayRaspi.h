d1b744a028dd2f6c68d51dbc60f784cbd26b58e3
Fditoro <fditoro@itba.edu.ar>
2022-10-31 15:23:12 -0300

    Prueba score 1

--- raspi/displayRaspi.h
+++ raspi/displayRaspi.h
@@ -81,7 +81,7 @@ typedef struct {//Este struct contiene la informacion necesaria para ejecutar un
 	keys_t * keys;
 	option_t selectOption[10];//Struct que contiene punteros a funciones que indican que hacer cuando se selecciona una opcion.
     char textOpciones[10][50];//Arreglo de punteros a los strings que contienen el texto de cada opcion.
-    halfDisp_t* drawingOpciones[10];//Arreglo de punteros a los dibujos que se deben mostrar en cada opcion.
+    halfDisp_t drawingOpciones[10][70];//Arreglo de punteros a los dibujos que se deben mostrar en cada opcion.
     int cantOpciones;//Cantidad de opciones del menu.
     int exitStatus;//Esta variable se utiliza para saber cuando hay que salir del thread.
     void (*changeOption)(void* argChangeOption);//Callback a la funcion que cambia la opcion seleccionada.
