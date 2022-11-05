60b4079e6ac30d1d114e591052fcb24de5012be1
Fditoro <fditoro@itba.edu.ar>
2022-11-05 13:57:56 -0300

    prueba barrido letras en display

--- raspi/scoreInRaspi.c
+++ raspi/scoreInRaspi.c
@@ -489,7 +489,7 @@ caracteres_t* alfabeto [43] =  {&letraA,&letraB,&letraC,&letraD,&letraE,&letraF,
 &letra6,&letra7,&letra8,&letra9,&letraGuion,&letraPunto,&letraEspacio,&alienChar1,&alienChar2,&alienChar3,&flechaScore};
 
 
-#define FIRSTROW(a) (((a)==1)? 0: 7)
+
 
 //**********************************************************************************************************************
 //**********************************************************************************************************************
@@ -498,103 +498,10 @@ caracteres_t* alfabeto [43] =  {&letraA,&letraB,&letraC,&letraD,&letraE,&letraF,
 //**********************************************************************************************************************
 //**********************************************************************************************************************
 
-void barridoLetra(char, char, int sentido, dcoord_t);
-void printLetra(caracteres_t);
-void copyMatrix(char,uint8_t [8][4]);
-int offsetAlfabeto(char );
-void sweepMatrix(uint8_t [8][4], int );
-void addRow(uint8_t [8][4], uint8_t [4], int );
 
 
-int main (){
-    dcoord_t coordenada = {0,0};
-    barridoLetra('f','e',-1,coordenada);
-    return 0;
-}
 
 
-void barridoLetra (char letraUno, char letraDos, int sentido, dcoord_t coordenada ){
-    int i;
-    uint8_t matriz [8][4];
-    uint8_t matrizCopy [8][4];
-    copyMatrix(letraUno, matriz);
-    copyMatrix(letraDos, matrizCopy);
-    printLetra(matriz);
-    usleep(2000000);
-    for (i = 0; i<8 ; i++){
-        sweepMatrix(matriz, sentido);
-        addRow(matriz,matrizCopy[FIRSTROW(sentido)+i*sentido],sentido);
-        printLetra(matriz);
-        usleep(2000000);
-    }
-    return;
-}
 
-void addRow(uint8_t matriz [8][4], uint8_t rowToAdd [4], int sentido){
-    int i;
-    for (i=0; i<4; i++){
-        matriz[FIRSTROW(-sentido)][i]=rowToAdd[i];
-    }
-    return;
-}
 
-void sweepMatrix(uint8_t matriz [8][4], int sentido){
-    int i,j;
-    for (j=0; j<7; j++){
-        for(i=0; i<4; i++){
-            matriz[FIRSTROW(sentido)+j*sentido][i]= matriz[FIRSTROW(sentido)+sentido+j*sentido][i];
-            //matriz[j][i]= matriz[j+1][i];
-        }
-    }
-}
 
-void copyMatrix(char letra, uint8_t matriz [8][4]){
-    int i,j;
-    for (j=0; j<8; j++){
-        for(i=0; i<4; i++){
-            matriz[j][i]= (*(alfabeto[offsetAlfabeto(letra)]))[j][i];
-        }
-    }
-
-    return;
-}
-
-void printLetra(caracteres_t letra){
-    int i,j;
-    for(j = 0; j<8; j++){
-        for(i = 0; i<4 ; i++){
-            printf("%s", letra[j][i]?"██":"  ");
-        }
-        printf("\n");
-    }
-    printf("\n\n");
-    return;
-}
-
-int offsetAlfabeto(char caracter){
-//Esta funcion se utiliza para obtener el offset necesario para acceder al string "alfabeto".
-    int offset;
-    
-    if(caracter >= 'a' && caracter <= 'z'){//Si es una letra minuscula.
-        offset = caracter - 'a' + OFFSETLETRA;
-    }
-    else if(caracter >= 'A' && caracter <= 'Z'){//Si es una letra mayuscula.
-        offset = caracter - 'A' + OFFSETLETRA;
-    }
-    else if(caracter >= '0' && caracter <= '9'){//Si es un numero.
-        offset = caracter - '0' + OFFSETNUM;
-    }
-    else if(caracter == '-'){//Si es un guion.
-        offset = OFFSETCHARESP;
-    }
-    else if(caracter == '.'){//Si es un punto.
-        offset = OFFSETCHARESP + 1;
-    }
-    else if(caracter == ' '){//Si es un espacio.
-        offset = OFFSETCHARESP + 2;
-    }
-    else if(caracter == '>'){//Si es un espacio.
-        offset = OFFSETCHARESP + 6;
-    }
-    return offset;
-}
