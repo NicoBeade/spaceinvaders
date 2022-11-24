4eabb094041dc8c0873b4637cd3bd6cb0126df9a
nbeade <nbeade@itba.edu.ar>
2022-11-24 11:44:20 -0300

    Se crearon los paths para todos los audios

--- raspi/audiosRaspi/audiosRaspi.h
+++ raspi/audiosRaspi/audiosRaspi.h
@@ -8,39 +8,37 @@ Si su ejemplo no utiliza threads, no es necesario linkear (-lpthread).*/
 #include "audio.h"
 #include <SDL2/SDL.h>
 
-
+#define PATH "audiosRaspi/audioFilesRaspi/"
 
 //Paths a las musicas del juego
-#define MUS_MENU
-#define MUS_JUEGO
+#define MUS_MENU (PATH "musica_menu.wav")
+#define MUS_JUEGO (PATH "musica_juego.wav")
 
 //Paths a los sonidos de las colisiones
-#define SON_COLISION_ALIEN_TOCADO
-#define SON_COLISION_ALIEN_MUERTO
-#define SON_COLISION_USER_TOCADO
-#define SON_COLISION_USER_MUERTO
-#define SON_COLISION_MOTHERSHIP_MUERTA
-#define SON_COLISION_BARRERA_TOCADA
-#define SON_COLISION_BARRERA_MUERTA
+#define SON_COLISION_ALIEN_TOCADO (PATH "alien_tocado.wav")
+#define SON_COLISION_ALIEN_MUERTO (PATH "alien_muerto.wav")
+#define SON_COLISION_USER_TOCADO (PATH "user_tocado.wav")
+#define SON_COLISION_USER_MUERTO (PATH "user_muerto.wav")
+#define SON_COLISION_MOTHERSHIP_MUERTA (PATH "mothership_muerta.wav")
+#define SON_COLISION_BARRERA_TOCADA (PATH "barrera_tocada.wav")
+#define SON_COLISION_BARRERA_MUERTA (PATH "barrera_muerta.wav")
 
 //Path al sonido de movimiento de los aliens
-#define SON_MOVIMIENTO_ALIENS
+#define SON_MOVIMIENTO_ALIENS (PATH "movimiento_aliens.wav")
 
 //Path al sonido de la aparicion de la mothership
-#define SON_MOTHERSHIP_APARECE
+#define SON_MOTHERSHIP_APARECE (PATH "mothership_aparece.wav")
 
 //Paths a los sonidos de las balas disparadas
-#define SON_BALA_ALIEN
-#define SON_BALA_USER
+#define SON_BALA_ALIEN (PATH "bala_alien.wav")
+#define SON_BALA_USER (PATH "bala_user.wav")
 
 //Paths a los sonidos de acciones dentro del menu
-#define SON_SELECT_MENU
-#define SON_SWAP_MENU
-#define SON_ERROR_MENU
-#define SON_SAVED_SCORE
-#define SON_SWEEP_LETRA
-
-
+#define SON_SELECT_MENU (PATH "select_menu.wav")
+#define SON_SWAP_MENU (PATH "user_tocado.wav")
+#define SON_ERROR_MENU (PATH "error_menu.wav")
+#define SON_SAVED_SCORE (PATH "saved_score.wav")
+#define SON_SWEEP_LETRA (PATH "sweep_letra.wav")
 
 //Enums de las musicas del juego
 enum MUSICA {MUSICA_MENU,MUSICA_JUEGO};
