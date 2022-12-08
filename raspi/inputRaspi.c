c879180284bc06a6d1762ac025ea48e13a44acff
nbeade <nbeade@itba.edu.ar>
2022-12-08 12:52:39 -0300

    prueba raspi

--- raspi/inputRaspi.c
+++ raspi/inputRaspi.c
@@ -32,23 +32,21 @@ void* inputRPIThread(void* argInputRPI){
 
     while(GAME_STATUS.exitStatus){
 
-        usleep(10 * U_SEC2M_SEC);//Espera 10mS para igualar el tiempo del timer.
-        if( (timerTick % velInput) == 0 ){//Determina cada cuanto se actualiza el input.
-            sem_wait(&SEM_DRIVER);
-            joy_update();
-            coordJoy = joy_get_coord();//Obtiene las coordenadas del joystick.
-            switchJoy =  joy_get_switch();
-            sem_post(&SEM_DRIVER);
+        usleep(10 * U_SEC2M_SEC * velInput);//Espera 10mS para igualar el tiempo del timer.
+        sem_wait(&SEM_DRIVER);
+        joy_update();
+        coordJoy = joy_get_coord();//Obtiene las coordenadas del joystick.
+        switchJoy =  joy_get_switch();
+        sem_post(&SEM_DRIVER);
 
-            //Si el joystick se mueve, indico ese valor en los campos de la variable keys.
-            ((keys_t*)argInputRPI) -> x = (coordJoy.x <= JOY_ACTIVE_NEG) ? -1 : ( (coordJoy.x >= JOY_ACTIVE_POS) ? 1 : 0); 
+        //Si el joystick se mueve, indico ese valor en los campos de la variable keys.
+        ((keys_t*)argInputRPI) -> x = (coordJoy.x <= JOY_ACTIVE_NEG) ? -1 : ( (coordJoy.x >= JOY_ACTIVE_POS) ? 1 : 0); 
 
-            ((keys_t*)argInputRPI) -> y = (coordJoy.y <= JOY_ACTIVE_NEG_Y) ? -1 : ( (coordJoy.y >= JOY_ACTIVE_POS) ? 1 : 0);
+        ((keys_t*)argInputRPI) -> y = (coordJoy.y <= JOY_ACTIVE_NEG_Y) ? -1 : ( (coordJoy.y >= JOY_ACTIVE_POS) ? 1 : 0);
 
-            //Esta seccion detecta si se presiono el boton del joystick.
-            ((keys_t*)argInputRPI) -> press = (switchJoy == J_PRESS) ? 1 : 0;
-            
-        }
+        //Esta seccion detecta si se presiono el boton del joystick.
+        ((keys_t*)argInputRPI) -> press = (switchJoy == J_PRESS) ? 1 : 0;
+
     }
     pthread_exit(0);
 }
